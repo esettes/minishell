5e58b9e217f150465e6c415fa1a5d30ae5bf2db9
esettes <ioanaroxana.st@gmail.com>
2024-04-17 21:33:30 +0200

    changes at file descriptors and dup

--- src/pipex/pipex.c
+++ src/pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: iostancu <iostancu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:08:23 by iostancu          #+#    #+#             */
-/*   Updated: 2024/04/11 23:32:53 by iostancu         ###   ########.fr       */
+/*   Updated: 2024/04/17 21:27:15 by iostancu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ int	f_pipex(t_pipe *p_data, t_cmd *cmd, char *envp[])
 	if (run_last_process(cmd, &p_data, envp, i))
 		return (EXIT_FAILURE);
 	n_cmds = cmd->n_scmd;
-	waitpid(p_data->pid, &g_signal, 0);
+	printf("before waitpid PID: %d\n", p_data->pid2);
+	waitpid(p_data->pid2, &g_signal, 0);
 	return (EXIT_SUCCESS);
 }
 
@@ -85,11 +86,15 @@ int	process_loop(t_cmd *cmd, char *envp[], t_pipe **p_data, int pos)
 	(*p_data)->pid = fork();
 	if ((*p_data)->pid < 0)
 		return (f_error());
+	
 	if ((*p_data)->pid == 0)
 		if (run_child(*p_data, cmd, envp, pos))
 			return (EXIT_FAILURE);
+	close((*p_data)->pip[W]);
 	close_files(&(*p_data)->infile, &(*p_data)->outfile);
-	(*p_data)->infile = (*p_data)->pip[R];
+	if ((*p_data)->infile > -1)
+		(*p_data)->infile = (*p_data)->pip[R];
+	close((*p_data)->pip[R]);
 	return (EXIT_SUCCESS);
 }
 
@@ -99,23 +104,24 @@ int	run_last_process(t_cmd *cmd, t_pipe **p_data, char *envp[], int pos)
 		return (f_error());
 	if (open_file(cmd, *p_data, pos))
 		return (EXIT_FAILURE);
-	if (ft_strncmp("cd", (*p_data)->last_cmd[0], sizeof("cd")) != 0
+	/*if (ft_strncmp("cd", (*p_data)->last_cmd[0], sizeof("cd")) != 0
 		&& ft_strncmp("exit", (*p_data)->last_cmd[0], sizeof("exit")) != 0
 		&& ft_strncmp("echo", (*p_data)->last_cmd[0], sizeof("echo")) != 0
 		&& ft_strncmp("export", (*p_data)->last_cmd[0], sizeof("export")) != 0
 		&& ft_strncmp("unset", (*p_data)->last_cmd[0], sizeof("unset")) != 0
 		&& ft_strncmp("env", (*p_data)->last_cmd[0], sizeof("env")) != 0)
-	{
+	{*/
 		(*p_data)->pid2 = fork();
 		if ((*p_data)->pid2 < 0)
 			return (f_error());
 		if ((*p_data)->pid2 == 0)
 			if (run_child2(*p_data, cmd, envp, pos))
 				return (EXIT_FAILURE);
-	}
+	/*}
 	else
 		if (run_parent(cmd, p_data, pos))
-			return (EXIT_FAILURE);
+			return (EXIT_FAILURE); */
 	close_files(&(*p_data)->infile, &(*p_data)->outfile);
+	//printf("before finish run PID: %d\n", (*p_data)->pid2);
 	return (EXIT_SUCCESS);
 }
