8fb2e454543325bc4bc64ec719e06aafde0f0896
settes <ioanaroxana.st@gmail.com>
2024-07-08 16:29:52 +0200

    Rox merge 2 1 (#8)
    * doing export with no args
    
    * doing no args export cmd
    
    * export with no args and some norminette
    
    * update
    
    * update export and unset
    
    * little fix in history. update export and unset
    
    * some norminette
    
    * update
    
    * exit status fixed. some norminette
    
    * update
    
    * norminette
    
    * +norminette
    
    * update
    
    * update
    
    * changes at file descriptors and dup
    
    * fix outfile
    
    * export not exports :)
    
    * update
    
    * update
    
    * export works again
    
    * changes at exec_cd
    
    * get_env_var changes
    
    * wexitstatus didnt catch correctly all exit states
    
    * update
    
    * subo todo
    
    * update
    
    * solved heredoc error and one leak
    
    * delete comment
    
    * fixing dups
    
    * upload one comment in signal
    
    * solved diferents problems
    
    * signals managed
    
    * solved some leaks
    
    * solved some problems with the expander and leaks
    
    * change one issue
    
    * delete some lines
    
    * solved norminette errors
    
    * solved ctrl-d leaks
    
    * solved cd error and too changes
    
    * update
    
    * update
    
    * updating dups and redirs
    
    * fixing dups, little progress
    
    * update
    
    * changing all pipex
    
    * update
    
    * workiiing! need little changes as the exit when run single cmd
    
    * update. managing in and out files when multiple cmds. added int status memb to t_pipe and started managing it. changed f_error
    
    * update open files
    
    * update
    
    ---------
    
    Co-authored-by: tonysanchez64 <tony64kx.me@gmail.com>
    Co-authored-by: Ioana roxana Stancu <iostancu@c1r16s3.42madrid.com>
    Co-authored-by: Ioana roxana Stancu <iostancu@c1r17s1.42madrid.com>
    Co-authored-by: Ioana roxana Stancu <iostancu@c1r17s3.42madrid.com>
    Co-authored-by: Ioana roxana Stancu <iostancu@c1r13s6.42madrid.com>

--- src/pipex/processes.c
+++ src/pipex/processes.c
@@ -6,7 +6,7 @@
 /*   By: settes <settes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 14:08:41 by iostancu          #+#    #+#             */
-/*   Updated: 2024/07/08 04:06:43 by settes           ###   ########.fr       */
+/*   Updated: 2024/07/08 07:10:04 by settes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,9 @@ int	exec_process(t_pipe *data, char **cmd)
 	if ((cmd_have_current_path(cmd[0]) || !path))
 	{
 		free_split(cmd);
+		dprintf(2, "minishell: command not found");
 		g_signal = 2;
-		return (2);
+		return (EXIT_FAILURE);
 	}
 	if (execve(path, cmd, data->envp_minish) < 0)
 		return (f_error(data));
@@ -40,63 +41,12 @@ int	exec_process(t_pipe *data, char **cmd)
 	return (EXIT_SUCCESS);
 }
 
-
-int	run_child(t_pipe *data, t_cmd *cmd, int pos, char *old_cwd)
-{
-	if (cmd->scmd[pos]->out_f)
-	{
-		if (cmd->scmd[pos]->append)
-			data->outfile = open(cmd->scmd[pos]->out_f,
-				O_WRONLY | O_CREAT | O_APPEND, 0644);
-		else
-			data->outfile = open(cmd->scmd[pos]->out_f,
-				O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (data->outfile < 0)
-			return (f_error(data));
-		dup2(data->outfile, STDOUT_FILENO);
-		close(data->outfile);
-		data->pip[W] = data->outfile;
-		close(data->pip[W]); // (?)
-	}
-	else
-	{
-		data->outfile = -1;
-		//data->pip[W] = STDOUT_FILENO;
-		
-	}
-	if (!cmd->scmd[pos]->in_f)
-	{
-		dup2(data->previous_out, STDIN_FILENO);
-		close(data->previous_out);
-	}
-	else
-	{
-		data->infile = open(cmd->scmd[pos]->in_f, O_RDONLY, 0644);
-		if (data->infile < 0)
-			return (f_error(data));
-		dup2(data->infile, STDIN_FILENO);
-		close(data->infile);
-	}
-	dup2(data->pip[R], STDIN_FILENO);
-	dup2(data->pip[W], STDOUT_FILENO);
-	close(data->pip[R]);
-	close(data->pip[W]);
-	dprintf(2, "STDOUT_FILENO: %d\n", STDOUT_FILENO);
-	data->previous_out = data->pip[W];
-	dprintf(2, "data->previous_out (loop): %d\n", data->previous_out);
-	if (is_parent_exec(data->cmd[0]))
-		run_parent(cmd, &data, pos, old_cwd);
-	else
-		exec_process(data, data->cmd);
-	return (EXIT_SUCCESS);
-}
-
 int	exec_cmd(t_cmd *cmd, t_pipe **p_data, int pos, char *old_cwd)
 {
 	if (f_strncmp(*cmd->scmd[pos]->args, "cd", sizeof("cd")) == 0)
 	{
 		if (exec_cd(*p_data, cmd, pos))
-			return (f_error(*p_data));
+			return (EXIT_FAILURE);
 	}
 	else if (f_strncmp(*cmd->scmd[pos]->args, "pwd", sizeof("pwd")) == 0)
 		pwd_handler(old_cwd);
@@ -167,7 +117,18 @@ int	run_single_cmd(t_pipe *data, t_cmd *cmd, int pos, char *old_cwd)
 		close(data->outfile);
 		data->outfile = 0;
 	}
-	exec_cmd(cmd, &data, pos, old_cwd);
+	// if (!is_parent_exec(cmd->scmd[pos]->args[0]))
+	// {
+		data->pid = fork();
+		if (data->pid < 0)
+			return (f_error(data));
+		if (data->pid == 0)
+			data->status = exec_cmd(cmd, &data, pos, old_cwd);
+	// }
+	// else
+	// 	run_parent(cmd, &data, 0, old_cwd);
+	close_fds(data);
+	return (data->status);
 }
 
 void close_fds(t_pipe *data)
@@ -222,84 +183,5 @@ int	run_multiple_cmd(t_pipe *data, t_cmd *cmd, char *old_cwd)
 		close_fds(data);
 	}
 	data->old_fd = 0;
-	return (EXIT_SUCCESS);
-}
-
-
-int	run_child2(t_pipe *data, t_cmd *cmd, int pos, char *old_cwd)
-{
-	if (data->n_cmds == 1)
-	{
-		if (cmd->scmd[pos]->in_f)
-		{
-			data->infile = open(cmd->scmd[pos]->in_f, O_RDONLY, 0644);
-			if (data->infile < 0)
-				return (f_error(data));
-			dup2(data->infile, STDIN_FILENO);
-			close(data->infile);
-		}
-		if (cmd->scmd[pos]->out_f)
-		{
-			if (cmd->scmd[pos]->append)
-				data->outfile = open(cmd->scmd[pos]->out_f,
-					O_WRONLY | O_CREAT | O_APPEND, 0644);
-			else
-				data->outfile = open(cmd->scmd[pos]->out_f,
-					O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (data->outfile < 0)
-				return (f_error(data));
-			dup2(data->outfile, STDOUT_FILENO);
-			close(data->outfile);
-		}
-	}
-	else
-	{
-		if (cmd->scmd[pos]->out_f)
-		{
-			if (cmd->scmd[pos]->append)
-				data->outfile = open(cmd->scmd[pos]->out_f,
-					O_WRONLY | O_CREAT | O_APPEND, 0644);
-			else
-				data->outfile = open(cmd->scmd[pos]->out_f,
-					O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (data->outfile < 0)
-				return (f_error(data));
-			dup2(data->outfile, STDOUT_FILENO);
-			close(data->outfile);
-		}
-		else
-		{
-			data->outfile = -1;
-			//data->pip[W] = STDOUT_FILENO;
-			
-		}
-		if (!cmd->scmd[pos]->in_f)
-		{
-			dprintf(2, "previous_out: %d\n", data->previous_out);
-			dup2(data->previous_out, STDIN_FILENO);
-			close(data->previous_out);
-		}
-		else
-		{
-			data->infile = open(cmd->scmd[pos]->in_f, O_RDONLY, 0644);
-			if (data->infile < 0)
-				return (f_error(data));
-			dup2(data->infile, STDIN_FILENO);
-			close(data->infile);
-		}
-	}
-	dup2(data->pip[R], STDIN_FILENO);
-	dup2(data->pip[W], STDOUT_FILENO);
-	if (data->cmd_counter == 1)
-	{
-		
-		close(data->pip[R]);
-		close(data->pip[W]);
-	}
-	if (is_parent_exec(data->cmd[0]))
-		run_parent(cmd, &data, pos, old_cwd);
-	else
-		exec_process(data, data->cmd);
-	exit(g_signal);
 	return (EXIT_SUCCESS);
 }
